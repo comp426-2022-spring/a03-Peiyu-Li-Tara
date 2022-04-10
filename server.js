418598da0c444c5a885649240c48bb4c107564f1
Peiyu-Li <peiyu1@ad.unc.edu>
2022-04-10 04:28:20 -0400

    implement dynamic port setting

--- server.js
+++ server.js
@@ -1,43 +1,8 @@
-/** Coin flip functions 
- * This module will emulate a coin flip given various conditions as parameters as defined below
- */
-
-/** Simple coin flip
- * 
- * Write a function that accepts no parameters but returns either heads or tails at random.
- * 
- * @param {*}
- * @returns {string} 
- * 
- * example: coinFlip()
- * returns: heads
- * 
- */
-
 function coinFlip() {
 let status = Math.round(Math.random())
 if (status == 1) return "heads"
 return "tails"
 }
-
-/** Multiple coin flips
- * 
- * Write a function that accepts one parameter (number of flips) and returns an array of 
- * resulting "heads" or "tails".
- * 
- * @param {number} flips 
- * @returns {string[]} results
- * 
- * example: coinFlips(10)
- * returns:
- *  [
-	'heads', 'heads',
-	'heads', 'tails',
-	'heads', 'tails',
-	'tails', 'heads',
-	'tails', 'heads'
-	]
-*/
 
 function coinFlips(flips) {
 const result = []
@@ -46,19 +11,6 @@ for (let i = 0; i < flips; i ++){
 }
 return result
 }
-
-/** Count multiple flips
- * 
- * Write a function that accepts an array consisting of "heads" or "tails" 
- * (e.g. the results of your `coinFlips()` function) and counts each, returning 
- * an object containing the number of each.
- * 
- * example: conutFlips(['heads', 'heads','heads', 'tails','heads', 'tails','tails', 'heads','tails', 'heads'])
- * { tails: 5, heads: 5 }
- * 
- * @param {string[]} array 
- * @returns {{ heads: number, tails: number }}
- */
 
 function countFlips(array) {
 let numHeads = 0;
@@ -69,17 +21,6 @@ for (let i = 0; i < array.length; i ++){
 }
 return {heads: numHeads, tails: numTails}
 }
-
-/** Flip a coin!
- * 
- * Write a function that accepts one input parameter: a string either "heads" or "tails", flips a coin, and then records "win" or "lose". 
- * 
- * @param {string} call 
- * @returns {object} with keys that are the input param (heads or tails), a flip (heads or tails), and the result (win or lose). See below example.
- * 
- * example: flipACoin('tails')
- * returns: { call: 'tails', flip: 'heads', result: 'lose' }
- */
 
 function flipACoin(call) {
 let flip = coinFlip()
@@ -99,22 +40,20 @@ summary = countFlips(flips)
 return {flips: flips, summary: summary}
 }
 
+
 const express = require('express');
 const app = express()
 
 // const arg = require('minimist').split(2)
 const args = require('minimist')(process.argv.slice(2))
-const port = args['port'] || 5000
+var port = args['port'] || process.env.PORT || 5000
 
-// Start an app server
-const server = app.listen(5000, () => {
+const server = app.listen(port, () => {
     console.log('App listening on port %port%'.replace('%port%',port))
 });
 
 app.get('/app', (req, res) => {
-    // Respond with status 200
     res.statusCode = 200;
-    // Respond with status message "OK"
     res.statusMessage = 'OK';
     res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
     res.end(res.statusCode+ ' ' +res.statusMessage)
@@ -140,7 +79,6 @@ app.get('/app/flip/call/tails', (req, res) => {
 	res.status(200).json({'call': 'tails', 'flip': flip.flip, 'result': flip.result})
 });
 
-// Default response for any other request
 app.use(function(req, res){
     res.status(404).send('404 NOT FOUND')
 });
